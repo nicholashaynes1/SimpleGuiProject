0cd174eb9f1bf851c0f42662d0076908e60a028d
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-11-09 12:19:16 -0700

    Added more documentation and added our GuiFrame reference to the controller in the contractor call.

--- src/gui/controller/GuiController.java
+++ src/gui/controller/GuiController.java
@@ -1,10 +1,27 @@
 package gui.controller;
 
+import gui.view.GuiFrame;
+
+/**
+ * Appcontroller for the simple gui project
+ * @author nhay7834
+ *
+ */
 public class GuiController
 {
-	public void start()
+	
+	/**
+	 * Reffereance to our GUIFrame object of the view.
+	 */
+	private GuiFrame baseFrame;
+	
+	public GuiController()
 	{
-		
+		baseFrame = new GuiFrame(this);
 	}
+	
+	
+	public void start()
+	{}
 		
 }
