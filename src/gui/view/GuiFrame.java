4ae5c0fa67a5984ef523fd2c7604b4f5d1fb8440
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-11-23 11:52:36 -0700

    Added our mouseListener Methods.
    Does nothing…yet.

--- src/gui/view/GuiFrame.java
+++ src/gui/view/GuiFrame.java
@@ -2,25 +2,26 @@ package gui.view;
 
 import javax.swing.JFrame;
 import gui.controller.GuiController;
+
 /**
  * Creates the frame for the gui project
+ * 
  * @author nhay7834
- *@version0.2 Nov 9, 2015 added setup frame helper method
+ * @version0.2 Nov 9, 2015 added setup frame helper method
  */
 public class GuiFrame extends JFrame
 {
 	private GuiController baseController;
 	private GuiPanel basePanel;
-	
+
 	public GuiFrame(GuiController baseController)
 	{
-		this.baseController = baseController; //Assigns parameter to the data member. 
+		this.baseController = baseController; // Assigns parameter to the data member.
 		basePanel = new GuiPanel(baseController);
 		setupFrame();
-		
-		
+
 	}
-	
+
 	/**
 	 * helper method to set up our frame.
 	 */
@@ -28,7 +29,7 @@ public class GuiFrame extends JFrame
 	{
 		this.setContentPane(basePanel);
 		this.setResizable(false);
-		this.setSize(500, 500); //Sets our size
+		this.setSize(500, 500); // Sets our size
 		this.setVisible(true);
 	}
 }
